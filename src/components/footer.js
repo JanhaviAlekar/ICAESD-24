c7cf74dd020947132dbb8652b225d43a8fab7050
JanhaviAlekar <janhavialekar@gmail.com>
2024-04-24 12:48:25 +0530

    alt changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
         <footer className="bg-white rounded-lg m-4 my-6">
          
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span class="text-sm text-gray-700 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">GCOEA</a>. All Rights Reserved.
+              <span class="text-sm text-gray-700 sm:text-center dark:text-gray-400">© 2023 <a href="www.google.com" class="hover:underline">GCOEA</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
