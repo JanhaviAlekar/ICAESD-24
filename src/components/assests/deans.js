aff8a209bc20f4c052cec9e75f2b50a9ec45615f
Devesh9321 <curiousforlearningtech@gmail.com>
2025-07-14 20:25:28 +0530

    profile image and designations of deans and students comity added
    here I update the deans section as well as students comity sections. where the updates related to profile pic's and designation. That changes will carried our base on convener's suggestion.

--- src/components/assests/deans.js
+++ src/components/assests/deans.js
@@ -1,41 +1,46 @@
-import user from "./test/user.png";
+import Chaudhari from "../assests/deans/R.R.Chaudhary.png";
+import MVJape from "../assests/deans/m_jape.jpeg";
+import Ghate from "../assests/deans/v_ghate.jpeg";
+import Thorkar from "../assests/deans/prof_u_thorkar.jpeg";
+import MMDeshmukh from "../assests/deans/MMDeshmukh.jpg";
+import Rmetkar from "../assests/organizing/Rmetkar.jpeg";
 
 const deans = [
   {
     sr_no: 1,
     name_of_deans: "Dr. R. R. Chaudhari",
     designation: "Dean, Academics",
-    img: user,
+    img: Chaudhari,
   },
   {
     sr_no: 2,
     name_of_deans: "Dr. R. M. Metkar",
     designation: "Dean, Research and Development",
-    img: user,
+    img: Rmetkar,
   },
   {
     sr_no: 3,
     name_of_deans: "Dr. M. V. Jape",
     designation: "Dean Students’ Welfare",
-    img: user,
+    img: MVJape,
   },
   {
     sr_no: 4,
-    name_of_deans: "Prof. M. M. Deshmukh,",
+    name_of_deans: "Prof. M. M. Deshmukh",
     designation: "Dean, Training and Placement",
-    img: user,
+    img: MMDeshmukh,
   },
   {
     sr_no: 5,
     name_of_deans: "Dr. V. N. Ghate",
     designation: "Dean, Planning and Administration",
-    img: user,
+    img: Ghate,
   },
   {
     sr_no: 6,
     name_of_deans: "Prof. U. M. Thorkar",
     designation: "Dean, Quality Assurance",
-    img: user,
+    img: Thorkar,
   },
 ];
 
